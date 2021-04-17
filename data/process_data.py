b9d73a96944415d6ce457b22c201132417e74f47
Khalid AlAwar <29180707+khalidalawar1@users.noreply.github.com>
2021-04-17 14:45:20 +0400

    ETL Pipeline implemented and run

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,32 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on="id")
+    return df
 
 def clean_data(df):
-    pass
+    categories = df['categories'].str.split(';', expand=True) # creating a dataframe of the 36 individual category columns
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda x: x[:-2])
+    categories.columns = category_colnames # renaming the columns of `categories`
+    for column in categories:
 
+        categories[column] = categories[column].apply(lambda x: x[-1])    # setting each value to be the last character of the string
+    
+        categories[column] = pd.to_numeric(categories[column]) # convert column from string to numeric
+    df.drop(['categories'], axis=1, inplace=True) #dropping the column where we derived new col names
+    df = pd.concat([df,categories], axis=1) # concatenating the original dataframe with the new `categories` dataframe
+    df.drop_duplicates(inplace=True)
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('InsertTableName', engine, index=False)  
 
 
 def main():
