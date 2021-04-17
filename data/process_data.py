75d54d22eff3e591a72a1a45f660a5672701bb2e
Khalid AlAwar <29180707+khalidalawar1@users.noreply.github.com>
2021-04-17 15:35:08 +0400

    fixed table name

--- data/process_data.py
+++ data/process_data.py
@@ -26,7 +26,7 @@ def clean_data(df):
 
 def save_data(df, database_filename):
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('InsertTableName', engine, index=False)  
+    df.to_sql('Message', engine, index=False)  
 
 
 def main():
