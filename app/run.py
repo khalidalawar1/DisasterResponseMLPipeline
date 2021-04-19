7f86221934bef25b4665720ef26022ab34825a86
Khalid AlAwar <29180707+khalidalawar1@users.noreply.github.com>
2021-04-19 14:39:21 +0400

    visualizations implemented and working

--- app/run.py
+++ app/run.py
@@ -8,7 +8,8 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+from plotly.graph_objs import Pie
+import joblib
 from sqlalchemy import create_engine
 
 
@@ -26,11 +27,13 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('Message', engine)
+
+
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -38,13 +41,18 @@ model = joblib.load("../models/your_model_name.pkl")
 @app.route('/index')
 def index():
     
-    # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
+    # extracting data needed for visuals
+    
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
+
+    top_10_counts = df.iloc[:,4:].sum(axis=0,skipna=True).sort_values(ascending=False)[1:10]
     
-    # create visuals
-    # TODO: Below is an example - modify to create your own visuals
+    isrelated_counts = df.groupby('related').count()['id']
+    
+
+    # creating visuals
+    
     graphs = [
         {
             'data': [
@@ -63,28 +71,58 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+        {
+            'data': [
+                Pie(
+                    values=top_10_counts.values,
+                    labels=top_10_counts.index
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Disaster Categories',
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=['unrelated', 'related with 1 disaster type', 'related with 2 disaster types'],
+                    y=isrelated_counts.values
+                )
+            ],
+
+            'layout': {
+                'title': 'Relative Messages Relations with Disaster',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Number of Disaster Relations of Message"
+                }
+            }
         }
     ]
     
-    # encode plotly graphs in JSON
+    # encoding plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
     
-    # render web page with plotly graphs
+    # rendering web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
 
 # web page that handles user query and displays model results
 @app.route('/go')
 def go():
-    # save user input in query
+    # saving user input in query
     query = request.args.get('query', '') 
 
-    # use model to predict classification for query
+    # using model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html
     return render_template(
         'go.html',
         query=query,
