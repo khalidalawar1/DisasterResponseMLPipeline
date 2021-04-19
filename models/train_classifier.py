42e68140b516336bfbe1909a70d3a4fcfb20a23b
Khalid AlAwar <29180707+khalidalawar1@users.noreply.github.com>
2021-04-19 14:15:04 +0400

    training classifier code

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,75 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report
+
+import numpy as np
+
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+import pickle
 
 
 def load_data(database_filepath):
-    pass
-
+    # load data from database
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('Message',engine)
+    X = df['message']
+    y = df.drop(['id','message','original','genre'], axis=1)
+    return X,y, y.columns
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    parameters = {
+        'vect__max_df': (0.5,1.0)
+    }
+    
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+   
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_pred = model.predict(X_test)
+
+    
+    i=0
+    for cat in category_names:
+        print(cat)
+        print(classification_report(Y_test.values[:,i], Y_pred[:,i]))
+        i = i+1
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
